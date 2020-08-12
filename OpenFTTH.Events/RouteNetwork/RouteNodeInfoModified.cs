c622173631ab83e2618496ada0c770786f497b79
Jesper Ladegaard <jesper@dax.dk>
2020-08-12 20:42:01 +0200

    Ids added to RouteNodeInfoModified and RouteSegmentInfoModified events

--- OpenFTTH.Events/RouteNetwork/RouteNodeInfoModified.cs
+++ OpenFTTH.Events/RouteNetwork/RouteNodeInfoModified.cs
@@ -8,12 +8,16 @@ namespace OpenFTTH.Events.RouteNetwork
 {
     public class RouteNodeInfoModified : RouteNetworkEvent
     {
+        private readonly Guid _nodeId;
+        public Guid NodeId => _nodeId;
+
         private readonly RouteNodeInfo? _routeNodeInfo;
         public RouteNodeInfo? RouteNodeInfo => _routeNodeInfo;
 
-        public RouteNodeInfoModified(string eventType, Guid eventId, string cmdType, Guid cmdId, bool isLastEventInCmd, Guid workTaskMrid, string userName, string? applicationName, string? applicationInfo, RouteNodeInfo? routeNodeInfo)
+        public RouteNodeInfoModified(string eventType, Guid eventId, string cmdType, Guid cmdId, bool isLastEventInCmd, Guid workTaskMrid, string userName, string? applicationName, string? applicationInfo, Guid nodeId, RouteNodeInfo? routeNodeInfo)
           : base(eventType, eventId, cmdType, cmdId, isLastEventInCmd, workTaskMrid, userName, applicationName, applicationInfo)
         {
+            _nodeId = nodeId;
             _routeNodeInfo = routeNodeInfo;
         }
     }
--- OpenFTTH.Events/RouteNetwork/RouteSegmentInfoModified.cs
+++ OpenFTTH.Events/RouteNetwork/RouteSegmentInfoModified.cs
@@ -8,12 +8,16 @@ namespace OpenFTTH.Events.RouteNetwork
 {
     public class RouteSegmentInfoModified : RouteNetworkEvent
     {
+        private readonly Guid _segmentId;
+        public Guid SegmentId => _segmentId;
+
         private readonly RouteSegmentInfo? _routeSegmentInfo;
         public RouteSegmentInfo? RouteSegmentInfo => _routeSegmentInfo;
 
-        public RouteSegmentInfoModified(string eventType, Guid eventId, string cmdType, Guid cmdId, bool isLastEventInCmd, Guid workTaskMrid, string userName, string? applicationName, string? applicationInfo, RouteSegmentInfo? routeSegmentInfo)
+        public RouteSegmentInfoModified(string eventType, Guid eventId, string cmdType, Guid cmdId, bool isLastEventInCmd, Guid workTaskMrid, string userName, string? applicationName, string? applicationInfo, Guid segmentId, RouteSegmentInfo? routeSegmentInfo)
           : base(eventType, eventId, cmdType, cmdId, isLastEventInCmd, workTaskMrid, userName, applicationName, applicationInfo)
         {
+            _segmentId = segmentId;
             _routeSegmentInfo = routeSegmentInfo;
         }
     }
