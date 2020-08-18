b83842cde7ff061006ff04af7ece285abd9967ae
Rune Nielsen <runeanielsen@live.dk>
2020-08-18 12:36:27 +0200

    fixes typo

--- OpenFTTH.Events/Core/Infos/LifecycleInfo.cs
+++ OpenFTTH.Events/Core/Infos/LifecycleInfo.cs
@@ -7,7 +7,7 @@ namespace OpenFTTH.Events.Core.Infos
     public class LifecycleInfo
     {
         private readonly DeploymentStateEnum? _deploymentState;
-        public DeploymentStateEnum? DepploymentState => _deploymentState;
+        public DeploymentStateEnum? DeploymentState => _deploymentState;
 
         private readonly DateTime? _installationDate;
         public DateTime? InstallationDate => _installationDate;
