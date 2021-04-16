0fbeb273918ce717657ad942fccf5ea19cc34b3b
Jesper Ladegaard <jesper@dax.dk>
2021-04-16 09:05:12 +0200

    Base event type for use in event store added

---
+++ OpenFTTH.Events/EventStoreBaseEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OpenFTTH.Events
+{
+    public abstract record EventStoreBaseEvent : IDomainEvent
+    {
+        public long EventSequenceNumber { get; init; }
+        public Guid EventId { get; init; }
+        public DateTime EventTimestamp { get; init; }
+        public string? ApplicationName { get; init; }
+        public string? ApplicationInfo { get; init; }
+        public string? UserName { get; init; }
+        public Guid WorkTaskId { get; init; }
+
+        public string EventType => this.GetType().Name;
+    }
+}
