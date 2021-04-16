dc28f21293627b0ee2c9e4bd57c60f1366795ad3
Jesper Ladegaard <jesper@dax.dk>
2021-04-16 13:24:23 +0200

    rename and make all properties nullable in EventStoreBaseEvent

--- OpenFTTH.Events/EventStoreBaseEvent.cs
+++ OpenFTTH.Events/EventStoreBaseEvent.cs
@@ -2,16 +2,15 @@
 
 namespace OpenFTTH.Events
 {
-    public abstract record EventStoreBaseEvent : IDomainEvent
+    public abstract record EventStoreBaseEvent
     {
-        public long EventSequenceNumber { get; init; }
-        public Guid EventId { get; init; }
-        public DateTime EventTimestamp { get; init; }
+        public long? IncitingEventSequenceNumber { get; init; }
+        public DateTime? IncitingEventTimestamp { get; init; }
+        public Guid? IncitingEventId { get; init; }
+        public Guid? IncitingCmdId { get; init; }
         public string? ApplicationName { get; init; }
         public string? ApplicationInfo { get; init; }
         public string? UserName { get; init; }
         public Guid WorkTaskId { get; init; }
-
-        public string EventType => this.GetType().Name;
     }
 }
