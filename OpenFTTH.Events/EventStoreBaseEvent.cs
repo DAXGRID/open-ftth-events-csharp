54f911a1cf0173a8dfb4540dc86b15be819241d7
Jesper Ladegaard <jesper@dax.dk>
2021-04-29 08:46:00 +0200

    simplify base event

--- OpenFTTH.Events/EventStoreBaseEvent.cs
+++ OpenFTTH.Events/EventStoreBaseEvent.cs
@@ -4,12 +4,9 @@ namespace OpenFTTH.Events
 {
     public abstract record EventStoreBaseEvent
     {
-        public long? IncitingEventSequenceNumber { get; init; }
-        public DateTime? IncitingEventTimestamp { get; init; }
+        public DateTime Timestamp { get; init; }
         public Guid? IncitingEventId { get; init; }
         public Guid? IncitingCmdId { get; init; }
-        public string? ApplicationName { get; init; }
-        public string? ApplicationInfo { get; init; }
         public string? UserName { get; init; }
         public Guid? WorkTaskId { get; init; }
     }
