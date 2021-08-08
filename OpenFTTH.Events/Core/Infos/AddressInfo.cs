e278138389b891c43bab23f100ef36f2de92a3d9
Jesper Ladegaard <jesper@dax.dk>
2021-08-08 16:56:45 +0200

    address info class added

---
+++ OpenFTTH.Events/Core/Infos/AddressInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OpenFTTH.Events.Core.Infos
+{
+    public class AddressInfo
+    {
+        /// <summary>
+        /// Local or official access address id
+        /// </summary>
+        public Guid? AccessAddressId { get; set; }
+
+        /// <summary>
+        /// Local or official unit address id
+        /// </summary>
+        public Guid? UnitAddressId { get; set; }
+
+        /// <summary>
+        /// Direction information etc.
+        /// </summary>
+        public string? Remark { get; set; }
+
+        public AddressInfo() { }
+
+        public AddressInfo(Guid? accessAddressId, Guid? unitAddressId, string? remark)
+        {
+            AccessAddressId = accessAddressId;
+            UnitAddressId = unitAddressId;
+            Remark = remark;
+        }
+    }
+}
