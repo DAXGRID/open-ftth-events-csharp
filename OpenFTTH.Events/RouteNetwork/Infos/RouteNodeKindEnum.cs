d717bc9b3172e8bfad0cb48668e70a1267bcfd50
Rune Nielsen <runenielsen@runbox.com>
2022-03-10 20:54:15 +0100

    upgrades to .net 6.0

--- OpenFTTH.Events/RouteNetwork/Infos/RouteNodeKindEnum.cs
+++ OpenFTTH.Events/RouteNetwork/Infos/RouteNodeKindEnum.cs
@@ -1,29 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OpenFTTH.Events.RouteNetwork.Infos
+﻿namespace OpenFTTH.Events.RouteNetwork.Infos
 {
     public enum RouteNodeKindEnum
     {
-        CentralOfficeBig, 
-        CentralOfficeMedium, 
-        CentralOfficeSmall, 
-        CabinetBig, 
-        CabinetSmall, 
-        ManHole, 
-        HandHole, 
-        ConduitClosure, 
-        ConduitClosureStraight, 
-        ConduitClosureFork, 
-        ConduitClosureBranchOff, 
-        ConduitConnector, 
-        ConduitConnectorStraight, 
-        ConduitConnectorBranchOff, 
-        ConduitEnd, 
-        SpliceClosure, 
-        BuildingAccessPoint, 
-        MultiDwellingUnit, 
+        CentralOfficeBig,
+        CentralOfficeMedium,
+        CentralOfficeSmall,
+        CabinetBig,
+        CabinetSmall,
+        ManHole,
+        HandHole,
+        ConduitClosure,
+        ConduitClosureStraight,
+        ConduitClosureFork,
+        ConduitClosureBranchOff,
+        ConduitConnector,
+        ConduitConnectorStraight,
+        ConduitConnectorBranchOff,
+        ConduitEnd,
+        SpliceClosure,
+        BuildingAccessPoint,
+        MultiDwellingUnit,
         SingleDwellingUnit
     }
 }
